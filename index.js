c7154b23f343dea89c6329cc4c071f166b7a1c25
builtbyjosh <pc.refuge@gmail.com>
2021-03-06 11:53:39 -0600

    commit tree b63d816d232b5bab0efc0cf977e6fcdae4c78315

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, str){
+    return drivers.filter (possibleMatch => possibleMatch.toLowerCase() === str.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(str.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, str){
+    return drivers.filter( i => i.name == str)
+}
